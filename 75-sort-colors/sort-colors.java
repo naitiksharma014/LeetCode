186ff89f5a9c7450c80db20c80571bcead57cd1a
naitiksharma014 <naitiksharma014@gmail.com>
2025-05-17 22:21:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (30.03%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+        // int sm=Integer.MIN_VALUE;
+        // for(int i=0;i<nums.length;i++){
+        //    sm=Math.min(sm,nums[i]); 
+        // }
+
+        
+    }
+}
