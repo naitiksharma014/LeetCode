513fba86b33af4fe8815ed78487058f4a43e232e
naitiksharma014 <naitiksharma014@gmail.com>
2025-05-31 22:48:31 +0530

    Time: 3 ms (88.12%) | Memory: 81.7 MB (78.46%) - LeetSync

---
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+
+        //TC: O(Q * n)
+        
+        int n=nums.length;
+        
+
+        //Step 1: Make Difference Array
+        int diff[]=new int[n];
+        for (int[] query : queries){
+            int start=query[0];
+            int end=query[1];
+            int x=1;
+
+            diff[start]+=x;
+            if(end+1<n){
+                diff[end+1]-=x;
+            
+            }
+            
+        }
+
+
+        //Step 2: Result Array
+        // int result[]=new int[n];
+        int currSum=0;
+        for(int i=0;i<n;i++){
+            currSum+=diff[i];
+            diff[i]=currSum;
+          
+        }
+
+        
+        //Step 3: Check
+        for(int i=0;i<n;i++){
+            if(nums[i]>diff[i])return false;
+        }
+        return true;
+        
+    }
+}
