ad941e2d63f442e43bfd707d75380c494275a90a
naitiksharma014 <naitiksharma014@gmail.com>
2025-06-08 22:42:32 +0530

    Time: 28 ms (54.72%) | Memory: 55.6 MB (23.71%) - LeetSync

---
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numSubseq(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n=nums.length;
+
+        int M = 1000000007;
+
+
+        int power[] = new int[n];
+        power[0] = 1;
+        for(int i=1;i<n;i++){
+            power[i] = ( power[i-1] * 2 ) % M;
+        }
+
+        int result = 0;
+
+        int lp = 0 , rp = n-1;
+
+        while(lp<=rp){
+            if(nums[lp] + nums[rp] <= target){
+                result = ( result % M + power[rp - lp] ) % M;
+                lp++;
+            }
+            else rp--;
+        }
+
+        return result;
+        
+    }
+}
