6021d9e0dbccc75879f50a76c9610da2700cccef
naitiksharma014 <naitiksharma014@gmail.com>
2025-06-27 22:28:16 +0530

    Time: 29 ms (77.16%) | Memory: 57.2 MB (87.70%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,7 +1,53 @@
 class Solution {
+    public void swap( int[] nums , int a , int b ){
+        int t = nums[a];
+        nums[a] = nums[b];
+        nums[b] = t;
+    }
+
+     public int partition( int[] nums , int L , int R ){
+        int  pivot = nums[L];
+        int i = L+1;
+        int j = R;
+
+        while( i <= j ){
+            if( nums[i] < pivot && nums[j] > pivot ){
+                swap( nums , i , j );
+                i++;
+                j--;
+            }
+            if( nums[i] >= pivot ){
+                i++;
+            }
+            if( nums[j] <= pivot ){
+                j--;
+            }
+        }
+        swap( nums , j , L );
+        return j;
+    }
+
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort( nums );
-        int n = nums.length;
-        return nums[n-k];
+       int n = nums.length;
+
+        int L = 0;
+        int R = n-1;
+        int pivot_idx = 0;
+
+        //Kth Largest = nums[k-1]
+        while( true ){
+            pivot_idx = partition( nums , L , R );
+
+            if( pivot_idx == k-1 ){
+                break;
+            }
+            else if( pivot_idx > k-1 ){
+                R = pivot_idx-1;
+            }
+            else {
+                L = pivot_idx+1;
+            }
+        }
+        return nums[pivot_idx]; 
     }
 }
