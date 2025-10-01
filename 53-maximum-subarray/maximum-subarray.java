5f4e7cfcea42479904c4b968caf3253a8404d3ea
naitiksharma014 <naitiksharma014@gmail.com>
2025-10-01 23:42:40 +0530

    Time: 1 ms (99.43%) | Memory: 56.3 MB (99.39%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,19 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        //KADANE ALGO
-
         int n = nums.length;
-        int currSum = 0;
-        int max = nums[0];
-        
-        for(int i: nums){
-            currSum += i;
-            max = Math.max( max , currSum );
-            if( currSum < 0 ){
-                currSum = 0;
-            }
+        int sum = 0 , res = Integer.MIN_VALUE;
+
+        for(int num : nums){
+
+            sum += num;
+            res = Math.max(res , sum);
+            if(sum < 0) sum = 0;
         }
 
-        return max;
+        return res;
     }
 }
