8ed5b7ea2e88d63a5a41d987a466e48590c9e1bc
naitiksharma014 <naitiksharma014@gmail.com>
2025-10-05 13:03:29 +0530

    Time: 25 ms (96.47%) | Memory: 51.3 MB (93.01%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,49 +1,40 @@
 class Solution {
-    public void twoSum( int[] nums , int k , List<List<Integer>> result , int tar ){
-        int i = k; int n = nums.length;
+    public void twoSum(int[] nums , int k , int target , ArrayList<List<Integer>> res){ 
+        int n = nums.length;
+        int i = k;
         int j = n - 1;
 
-        while( i < j ){
-            if( nums[i] + nums[j] > tar ){
-                j--;
-            }
-            else if( nums[i] + nums[j] < tar ){
-                i++;
-            }
-            else{
-                result.add( Arrays.asList( -tar , nums[i] , nums[j] ) );
+        while(i < j){
 
-                while( i < j && nums[i] == nums[i + 1] ){
-                    i++;
-                } 
-                while( i < j && nums[j] == nums[j - 1] ){
-                    j--;
-                }
-                
+            if(nums[i] + nums[j] > target) j--;
+            else if(nums[i] + nums[j] < target) i++;
+            else{
+                res.add(Arrays.asList(- target , nums[i] , nums[j]) );
+
+                while(i < j && nums[i] == nums[i + 1]) i++;
+                while(i < j && nums[j] == nums[j - 1]) j--;
+
                 i++;
                 j--;
             }
-            
         }
     }
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
-       
-       if( n < 3 ){
-           return new ArrayList<>();
+        
+        if(n < 3) return new ArrayList<>();
+        
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int i = 0 ; i < n - 2 ; i++){
+
+            if(i != 0 && nums[i] == nums[i - 1]) continue;
+            int target = nums[i];
+
+            twoSum(nums , i + 1 , - target , res);
         }
 
-       List<List<Integer>> result = new ArrayList<>();
-       Arrays.sort( nums );
-
-       for( int i = 0 ; i < n - 2 ; i++ ){
-            if( i != 0 && nums[i] == nums[i - 1] ){
-             continue;
-            }
-            twoSum( nums , i + 1 , result , - nums[i] );
-
-        }
-
-       return result;
+        return res;
     }
 }
