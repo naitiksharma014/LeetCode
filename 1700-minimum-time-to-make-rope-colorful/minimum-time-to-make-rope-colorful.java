204079d59107f9f16d78baa5811e495741b01885
naitiksharma014 <naitiksharma014@gmail.com>
2025-11-03 21:21:57 +0530

    Time: 8 ms (82.68%) | Memory: 90.4 MB (5.68%) - LeetSync

---
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        int n = colors.length();
+        int totalTime=0;
+
+        for(int i = 0; i < n; i++){
+           
+            if (i > 0 && colors.charAt(i) == colors.charAt(i - 1)) {
+                
+                totalTime += Math.min(neededTime[i], neededTime[i - 1]);
+               
+                neededTime[i] = Math.max(neededTime[i], neededTime[i - 1]);
+            }
+        }
+
+        return totalTime;
+    } 
+}
