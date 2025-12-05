28ff14fd84745abfb3853d66edf8e265418708cb
naitiksharma014 <naitiksharma014@gmail.com>
2025-12-05 19:43:00 +0530

    count-partitions-with-even-sum-difference

--- 3432-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
+++ 3432-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
@@ -1,3 +1,7 @@
+//------------------------------------------------------------ (APPROACH - 1)-------------------------------------------------------------------------------------
+//One Pass Solution
+// O(n)
+
 class Solution {
     public int countPartitions(int[] nums) {
         int n = nums.length;
@@ -11,5 +15,35 @@ class Solution {
             return n - 1;
         }
         return 0;
+    }
+}
+
+//------------------------------------------------------------ (APPROACH - 2)-------------------------------------------------------------------------------------
+//Two Pass Solution
+//O(2n)
+
+
+    class Solution {
+    public int countPartitions(int[] nums) {
+        int n = nums.length;
+        int totalSum = 0;
+
+        for(int num : nums){
+            totalSum += num;
+        }
+
+        int sum = 0;
+        int count = 0;
+        
+        for(int i=0; i<n-1; i++){
+            int num = nums[i];
+            sum += num;
+
+            if(Math.abs(2 * sum - totalSum) % 2 == 0){
+                count++;
+            }
+        }
+
+        return count;
     }
 }
