e58d1b02a5b3ec96e2dadb495f970be7c523b5ff
naitiksharma014 <naitiksharma014@gmail.com>
2025-12-14 22:45:06 +0530

    Time: 2 ms (92.22%) | Memory: 70.9 MB (45.91%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,21 +1,43 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n=nums.length;
-        int [] prefix = new int[n];
-        prefix[0] = 1;
-        
-        //Prefix Array
-        for(int i=1;i<n;i++){
-           prefix[i] = prefix[i-1] * nums[i-1];
+        int prod = 1;
+        int n = nums.length;
+
+        int[] zeroArr = new int[n];
+        int zero = 0;
+
+        for(int i = 0; i < n; i++){
+            
+            if(nums[i] == 0){
+                zeroArr[i]++;
+                zero++;
+            }
+            if(nums[i] != 0){
+                prod *= nums[i];
+            }
+                 
         }
 
-        int suffix = 1;
-        for(int i=n-2;i>=0;i--){
-            suffix *= nums[i+1];
-            prefix[i] *= suffix;
+        for(int i = 0; i < n; i++){
+            
+            if(zero > 1){
+                nums[i] = 0;
+            }
+            else if(zero == 1){
+               //nums[i] = (zeroArr[i] > 0) ? prod : 0;
+                if (zeroArr[i] > 0) {
+                    nums[i] = prod;
+                } else {
+                     nums[i] = 0;
+                }
+            }
+            else {
+                int temp = prod / nums[i];
+                nums[i] = temp;
+            }
+           
         }
-        return prefix;
-        
 
+        return nums;
     }
 }
