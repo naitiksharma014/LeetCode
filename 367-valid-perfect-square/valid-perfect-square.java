0deee31007bd7909c95fc4831b9732582c874952
naitiksharma014 <naitiksharma014@gmail.com>
2026-01-05 23:13:05 +0530

    Add multiple approaches to check perfect square
    Refactored isPerfectSquare method to include three approaches: brute force, binary search, and Newton's method.

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -1,3 +1,30 @@
+//----------------------------------------------------------(APPROACH - 1)---------------------------------------------------------------------------------
+// TC: O(num)
+
+
+
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num == 1) return true;
+        
+        int n = num / 2;
+
+        for(int i = 1; i <= n; i++){
+
+            if(i * i == num) return true;
+        }
+
+        return false;
+    }
+}
+
+
+
+//----------------------------------------------------------(APPROACH - 2)---------------------------------------------------------------------------------
+// TC: O(log n)
+// Binary Search
+
+
 class Solution {
     public boolean isPerfectSquare(int num) {
         if(num == 1){
@@ -28,3 +55,26 @@ class Solution {
         return false;
     }
 }
+
+
+
+
+//----------------------------------------------------------(APPROACH - 3)---------------------------------------------------------------------------------
+// TC: O(log n)
+// Newton Method Code
+
+
+
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long x = num;
+
+        while (x * x > num) {
+            x = (x + num / x) / 2;
+        }
+
+        return x * x == num;
+    }
+}
+
+
