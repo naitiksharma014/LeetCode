48c5bf12a8bb6cab356153cfd0ccfc92f08dfae5
naitiksharma014 <naitiksharma014@gmail.com>
2026-01-14 21:11:00 +0530

    Generate Parenthesis Two Brute and Optimal Approach
    Added a second approach for generating parentheses with optimized time complexity.

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,3 +1,69 @@
+//-------------------------------------------------------------- ( APPORACH - 1) ----------------------------------------------------------------------------
+// TC: O(n * 4 ^ n)
+// SC: O( n)
+
+
+class Solution {
+    List<String> list = new ArrayList<>();
+    public boolean isValid(StringBuilder s){
+        Stack<Character> st = new Stack<>();
+
+        if(s.charAt(0) == ')') return false;
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(ch == '('){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()) return false;
+
+                st.pop();
+            }
+        }
+
+        return st.isEmpty();
+    }
+
+    public void solve(int len, StringBuilder s){
+        if(len == 0){
+
+            if(isValid(s)){
+                list.add(s.toString());
+            }
+
+            return;
+        }
+
+        solve(len - 1, new StringBuilder(s).append('('));
+        solve(len - 1, new StringBuilder(s).append(')'));
+    }
+
+    public List<String> generateParenthesis(int n){
+
+        solve(2 * n, new StringBuilder());   
+
+        return list; 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+    
+//---------------------------------------------------------------( APPORACH - 2)-----------------------------------------------------------------------------
+// TC: O( (4 ^ n) / sq. root(n) ) 
+// Sc: O(n)
+
+
+
 class Solution {
     List<String> list = new ArrayList<>();
 
