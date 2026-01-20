89f813d7d3e9e4f4390a29404b9b01a78e3b2780
naitiksharma014 <naitiksharma014@gmail.com>
2026-01-20 22:31:23 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (49.12%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
@@ -5,19 +5,25 @@ class Solution {
 
         for(int i = 0; i < n; i++){
 
-            boolean found = false;
-
-            for(int j = 1; j < nums.get(i); j++){
-
-                if( (j | (j + 1)) == nums.get(i)){
-                    ans[i] = j;
-                    found = true;
-                    break;
-                }
+            if(nums.get(i) % 2 == 0){
+                ans[i] = -1;
+                continue;
             }
 
-            if(!found){
-                ans[i] = -1;
+            for(int j = 1; j < 32; j++){
+
+                // Skip set bits(1) to get 0
+                if((nums.get(i) & (1 << j)) > 0){
+                    continue;
+                }
+
+                int prev = j - 1;
+
+                // Make prev to 0: use XOR
+                int x = nums.get(i) ^ (1 << (j - 1));
+
+                ans[i] = x;
+                break;
             }
         }
 
