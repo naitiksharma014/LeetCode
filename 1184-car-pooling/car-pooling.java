07c98548c1d3ee72d51684dd013b73391f619d9f
naitiksharma014 <naitiksharma014@gmail.com>
2026-02-02 21:43:05 +0530

    Car pooling solved by Line Sweep Technique and Difference Array
    Implemented a second approach using a difference array for car pooling.

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -1,3 +1,9 @@
+//-------------------------------------------------------------------------- (APPROACH - 1)---------------------------------------------------------------------------------------------------------------
+// Line Sweep Technique
+// Time Complexity = O(N log N) 
+// Space Complexity = O(N)
+
+
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
         int m = trips.length;
@@ -21,6 +27,50 @@ class Solution {
         for(int event: events.values()){
 
             totalCapacity += event;
+
+            if(totalCapacity > capacity){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+
+
+//-------------------------------------------------------------------------- (APPROACH - 2)---------------------------------------------------------------------------------------------------------------
+// Difference Array
+// Time Complexity = O(N)
+// Space Complexity = O(1001) = O(1)
+
+
+
+
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int m = trips.length;
+        int n = trips[0].length;
+
+        int[] diffArray = new int[1001];
+
+        for(int[] trip: trips){
+
+            int numPassengers = trip[0];
+            int from          = trip[1];
+            int to            = trip[2];
+
+            diffArray[from] += numPassengers;
+            diffArray[to] -= numPassengers;
+        }
+
+
+        int totalCapacity = 0;
+
+        for(int i = 0; i < 1001; i++){
+
+            totalCapacity += diffArray[i];
 
             if(totalCapacity > capacity){
                 return false;
