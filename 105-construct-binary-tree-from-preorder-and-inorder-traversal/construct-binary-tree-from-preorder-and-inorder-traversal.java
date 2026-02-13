52944cbeb42153ea7bb0fdab73c468277fb689d1
naitiksharma014 <naitiksharma014@gmail.com>
2026-02-13 23:54:45 +0530

    Two Approaches for binary tree construction
    Added comments for two approaches to constructing a binary tree from preorder and inorder traversals, including time and space complexity.

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,6 +13,59 @@
  *     }
  * }
  */
+
+/*
+------------------------------------------------------------------------------(APPROACH - 1)----------------------------------------------------------------------------------------------------
+Basic
+TC: O(n²)
+
+*/
+
+
+class Solution {
+    Map<Integer, Integer> map = new HashMap<>();
+    int idx = 0;
+
+    public TreeNode solve(int[] preorder, int[] inorder, int start, int end){
+        if(start > end){
+            return null;
+        }
+        
+        int rootVal = preorder[idx];
+        
+        int i = start;
+        for(; i < end; i++){
+            if(inorder[i] == rootVal){
+                break;
+            }
+        }
+       
+        idx++;
+        TreeNode root = new TreeNode(rootVal);
+
+        root.left = solve(preorder, inorder, start, i - 1);
+        root.right = solve(preorder, inorder, i + 1, end);
+
+        return root;
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        
+        return solve(preorder, inorder, 0, n - 1);
+    }
+}
+
+
+
+/*
+------------------------------------------------------------------------------(APPROACH - 2)----------------------------------------------------------------------------------------------------
+Optimal
+TC: O(n)
+SC: O(n)
+*/
+
+
 class Solution {
     int idx = 0;
     Map<Integer, Integer> map = new HashMap<>();
