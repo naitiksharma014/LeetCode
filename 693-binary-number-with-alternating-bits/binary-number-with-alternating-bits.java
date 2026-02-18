7b7ea5c66450397a8e0090ddd5a2f1454980d3a4
naitiksharma014 <naitiksharma014@gmail.com>
2026-02-18 21:56:27 +0530

    Implement multiple approaches for alternating bits check
    Added three approaches to check for alternating bits in a binary number.

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.java
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.java
@@ -1,3 +1,8 @@
+//----------------------------------------------------------------------(Approach - 1)-----------------------------------------------------------------
+// TC: O(log n)
+// SC: O(log n)
+
+
 class Solution {
     public boolean hasAlternatingBits(int n) {
         String bits = Integer.toBinaryString(n);
@@ -13,3 +18,41 @@ class Solution {
 
     }
 }
+
+
+
+//----------------------------------------------------------------------(Approach - 2)-----------------------------------------------------------------
+// TC: O(log n)
+// SC: O(1)
+
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int prev = -1;
+
+        while(n > 0){
+
+            int curr = n % 2;
+            if(prev == curr) return false;
+            prev = curr;
+            n = n / 2;
+        }
+
+        return true;
+    }
+}
+
+
+
+//----------------------------------------------------------------------(Approach - 3)-----------------------------------------------------------------
+// TC: O(1)
+// SC: O(1)
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int x = n ^ (n >>> 1);  // unsigned shift
+        return (x & (x + 1)) == 0;
+    }
+}
+
+
