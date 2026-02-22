ff6a7ffb95c6c1de1ba390a864f3b40fe51378ef
naitiksharma014 <naitiksharma014@gmail.com>
2026-02-22 11:42:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (14.06%) - LeetSync

--- 899-binary-gap/binary-gap.java
+++ 899-binary-gap/binary-gap.java
@@ -1,18 +1,28 @@
+// TC: O(log n) 
+// SC: O(1)
+
 class Solution {
-    public int binaryGap(int n) {
-        String binary = Long.toBinaryString(n);
-
+    public int binaryGap(int n){
+        
         int gap = 0;
-        int prev = 0;
+        int curr = 0;
+        int prev = -1;
 
-        for(int i = 0; i < binary.length(); i++){
+        while(n > 0){
 
-            int curr = i;
+            int currBit = (n & 1);   // check current (rightmost) bit
 
-            if(binary.charAt(curr) == '1'){
-                gap = Math.max(gap, curr - prev);
+            if(currBit == 1){
+               
+                if (prev != -1) {
+                    gap = Math.max(gap, curr - prev);
+                }
+               
                 prev = curr;
             }
+
+            n = n >> 1;
+            curr++;
         }
 
         return gap;
