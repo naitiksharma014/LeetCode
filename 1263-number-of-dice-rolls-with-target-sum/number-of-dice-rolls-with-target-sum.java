3a0a57e98733dda776a743ba8837f19eedf2d571
naitiksharma014 <naitiksharma014@gmail.com>
2026-03-07 13:49:51 +0530

    Top-Down and Bottom-Up
    Implemented a top-down dynamic programming approach with memoization for calculating the number of ways to roll dice to achieve a target sum. Added comments for clarity and maintained the bottom-up approach as a reference.

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,4 +1,50 @@
-// Bottom Up
+//-------------------------------------------------------------------------------------------------(Approach - 1)-------------------------------------------------------------------------------------------
+// Top - Down (DP + Memorization)
+// TC: O(n × target × k)
+// SC: O(n × target)
+
+
+class Solution {
+    int MOD = 1000000007;
+    int[][] dp;
+
+    public int solve(int n, int k, int target){
+        if(target < 0){
+            return 0;
+        }
+
+        if(n == 0){
+            return target == 0 ? 1 : 0;
+        }
+
+        if(dp[n][target] != -1){
+            return dp[n][target];
+        }
+
+        int ways = 0;
+        for(int faces = 1; faces <= k; faces++){
+            ways = (ways + solve(n - 1, k, target - faces)) % MOD;
+        }
+
+        return dp[n][target] = ways;
+    }
+
+    public int numRollsToTarget(int n, int k, int target) {
+        dp = new int[31][1001];
+        for(int[] row: dp){
+            Arrays.fill(row, -1);
+        }
+
+        return solve(n, k, target);
+    }
+}
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------(Approach - 2)-------------------------------------------------------------------------------------------
+// Bottom - Up
 // TC: O(n × target × k)
 // SC: O(n × target)
 
