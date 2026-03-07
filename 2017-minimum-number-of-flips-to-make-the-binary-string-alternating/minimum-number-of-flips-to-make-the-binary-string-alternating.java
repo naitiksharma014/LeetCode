a62147df2f5dd5671a185488bf6f5a16683b8e09
naitiksharma014 <naitiksharma014@gmail.com>
2026-03-07 22:37:26 +0530

    Solution with Better and Optimal

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -1,3 +1,70 @@
+//---------------------------------------------------------------------------------------------------(Approach - 2)-----------------------------------------------------------------------------------------------------------
+// Better
+// TC: O(n)
+// SC: O(n)
+
+class Solution {
+    public int minFlips(String s) {
+        int n = s.length();
+
+        // Two New Alternating String Starting with 1 and 0
+        StringBuilder s1 = new StringBuilder();   // 010101010101
+        StringBuilder s2 = new StringBuilder();   // 101010101010
+
+        for(int i = 0; i < 2 * n; i++){
+
+            if(i % 2 == 0){
+                s1.append('0');
+                s2.append('1');
+            } else {
+                s1.append('1');
+                s2.append('0');
+            }
+        }
+
+        int result = Integer.MAX_VALUE;
+        int flip1 = 0, flip2 = 0;
+        int left = 0, right = 0;
+
+        while(right < 2 * n){
+
+            if(s.charAt(right % n) != s1.charAt(right)){
+                flip1++;
+            }
+
+            if(s.charAt(right % n) != s2.charAt(right)){
+                flip2++;
+            }
+
+            //Shrink the window
+            while(right - left + 1 > n){
+                  
+                if(s.charAt(left % n) != s1.charAt(left)){
+                    flip1--;
+                }
+
+                if(s.charAt(left % n) != s2.charAt(left)){
+                    flip2--;
+                }
+
+                left++;
+            }
+            
+            // Valid Window
+            if(right - left + 1 == n){
+                result = Math.min(result, Math.min(flip1, flip2));
+            }
+
+            right++;
+        }
+
+        return result;
+    }
+}
+
+
+//---------------------------------------------------------------------------------------------------(Approach - 2)-----------------------------------------------------------------------------------------------------------
+// Optimal
 // TC: O(2n) = O(n)
 // SC: O(1)
 
