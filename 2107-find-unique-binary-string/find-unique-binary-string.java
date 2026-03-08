4c2ad6c535119d0c83d7b4b3f0f68816f1aee85a
naitiksharma014 <naitiksharma014@gmail.com>
2026-03-08 19:54:44 +0530

    Brute Force and Optimal (Diagonalization Technique)
    Added brute force approach for finding unique binary string.

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,4 +1,37 @@
-// Diagonalization Technique
+//---------------------------------------------------------------------------------------(Approach - 1)----------------------------------------------------------------------------------------------------------------------
+// Brute Force
+//T.C : O(n^2) - Iterating on each string and converting each character to integer
+//S.C : O(n) - Using set
+
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (String num : nums) {
+            set.add(Integer.parseInt(num, 2));
+        }
+        
+        int n = nums.length;
+        
+        String result = "";
+        
+        for (int number = 0; number <= n; number++) {
+            if (!set.contains(number)) {
+                result = Integer.toBinaryString(number);
+                while (result.length() < n) { //to make till length n
+                    result = "0" + result;
+                }
+
+                return result;
+            }
+        }
+        
+        return "";
+    }
+}
+
+//---------------------------------------------------------------------------------------(Approach - 2)----------------------------------------------------------------------------------------------------------------------
+// Optimal: Diagonalization Technique
 // TC: O(n)
 
 class Solution {
