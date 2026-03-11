c878b82eb498d9816094b5208322543559cf49d1
naitiksharma014 <naitiksharma014@gmail.com>
2026-03-11 20:52:00 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (82.81%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -0,0 +1,12 @@
+// TC: O(1)
+
+class Solution {
+    public int bitwiseComplement(int n) {
+        if(n == 0) return 1;
+
+        int bits = (int) Math.floor(Math.log(n) / Math.log(2)) + 1;
+        int mask = (1 << bits) - 1;
+
+        return n ^ mask;
+    }
+}
