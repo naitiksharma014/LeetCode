0f2ccb33e6a3331addbd2659b62dd97f823c2e74
naitiksharma014 <naitiksharma014@gmail.com>
2026-03-11 20:54:47 +0530

    Brute and Optimal

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -1,3 +1,33 @@
+//------------------------------------------------------------------------(Approach - 1)-----------------------------------------------------------------------------------------------------------------------
+// Brute
+// ⚙️ TC: O(log n)
+// 💾 SC: O(log n)
+
+class Solution {
+    public int bitwiseComplement(int n) {
+        if(n == 0) return 1;
+       
+        String binary = Integer.toBinaryString(n);  // O(log n)
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch: binary.toCharArray()){         // O(log n)
+            
+            if(ch == '0') {
+                sb.append('1');
+            } else {
+                sb.append('0');
+            }
+        }
+
+        return Integer.parseInt(sb.toString(), 2);   // O(log n)
+    }
+}
+
+
+
+
+//------------------------------------------------------------------------(Approach - 1)-----------------------------------------------------------------------------------------------------------------------
+// Optimal
 // TC: O(1)
 
 class Solution {
