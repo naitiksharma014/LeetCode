7fc88876c71060c6d907b1532728fcb988e89f3d
naitiksharma014 <naitiksharma014@gmail.com>
2026-04-15 21:47:34 +0530

    sortColors method with better and optimal algorithm
    Refactor sortColors method to implement a better approach using the Dutch National Flag algorithm.

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,4 +1,38 @@
-// Dutch National Flag (DNF) Algorithm: 3 Pointer Approach
+//--------------------------------------------------------------------(APPROACH-1)-------------------------------------------------------------------
+// Better Approach
+// ⏱️ TC: O(4 * n)
+// 💾 SC: O(1)
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int i=0,j=0,k=0;
+        
+
+        for(int l=0;l<nums.length;l++){
+            if(nums[l]==0)i++;
+            else if(nums[l]==1)j++;
+            else k++;
+        }
+
+        for(int l=0;l<i;l++){
+            nums[l]=0;
+        }
+         for(int l=i;l<i+j;l++){
+            nums[l]=1;
+        }
+         for(int l=i+j;l<i+j+k;l++){
+            nums[l]=2;
+        }
+        // Arrays.sort(nums);
+
+       
+        
+    }
+}
+
+
+//--------------------------------------------------------------------(APPROACH-2)-------------------------------------------------------------------
+// Optimal: Dutch National Flag (DNF) Algorithm: 3 Pointer Approach
 // ⏱️ TC: O(n)
 // 💾 SC: O(1)
 
