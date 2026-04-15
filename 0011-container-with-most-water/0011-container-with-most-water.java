ed9152025a04e3ad7f592da9d824885e7b23d21e
naitiksharma014 <naitiksharma014@gmail.com>
2026-04-15 21:21:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,29 @@
+// Two Pointer Approach (Greedy)
+// ⏱️ TC: O(n)
+// 💾 SC: O(1)
+
+class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int maxArea = 0;
+        int i = 0;
+        int j = n - 1;
+
+        while(i < j) {
+
+            int l = Math.min(height[i], height[j]);
+            int w = j - i;
+            int area = l * w;
+
+            maxArea = Math.max(maxArea, area);
+
+            if(height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        return maxArea;
+    }
+}
