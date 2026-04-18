39160867370cb61d99201846cc42444c88c789fa
naitiksharma014 <naitiksharma014@gmail.com>
2026-04-18 15:24:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,56 @@
+// 🧠 Binary Search in 2D
+// ⏱️ Time: O(log(m * n))
+// 📦 Space: O(1)
+
+class Solution {
+
+    public boolean searchInRow(int[][] matrix, int row, int target, int n) {
+        int left = 0, right = n - 1;
+
+        while(left <= right) {
+            int mid = left + (right + left) / 2;
+
+            if(matrix[row][mid] == target) {
+
+                return true;
+
+            } else if(matrix[row][mid] > target) {
+
+                left = mid + 1;
+
+            } else {
+
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int startRow = 0, endRow = m - 1;
+
+        while(startRow <= endRow) {
+
+            int midRow = startRow + (endRow - startRow) / 2;
+
+            if(matrix[midRow][0] <= target && matrix[midRow][n - 1] >= target) {
+                // search in the row
+                return searchInRow(matrix, midRow, target, n);
+
+            } else if(matrix[midRow][0] < target) {
+
+                startRow = midRow + 1;
+
+            } else {
+
+                endRow = midRow - 1;
+            }
+        }
+
+        return false;
+    }
+}
