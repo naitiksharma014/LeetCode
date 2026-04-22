e079e6a6429436b15b76adfc53434d27a6644e88
naitiksharma014 <naitiksharma014@gmail.com>
2026-04-22 21:00:23 +0530

    set matrix zeroes with better and optimal approach
    Implement setZeroes method to set matrix rows and columns to zero based on zero elements.

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,3 +1,39 @@
+//---------------------------------------------------(Approach - 1)-------------------------------------------------------------
+// Better
+// TC: O(m × n)
+// Sc: O(m + n)
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                
+                if(matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                
+                if(row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+
+
+//---------------------------------------------------(Approach - 2)-------------------------------------------------------------
 // Optimal Approach
 // TC:  O(m × n)
 // SC: O(1)
