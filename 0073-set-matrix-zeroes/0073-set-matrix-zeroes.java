08067539779a18122d4dba7dcf4799e73bcb8af3
naitiksharma014 <naitiksharma014@gmail.com>
2026-04-22 20:58:26 +0530

    Time: 1 ms (88.69%), Space: 47.4 MB (84.12%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,32 +1,58 @@
-//---------------------------------------------------(Approach - 1)-------------------------------------------------------------
-// Better
-// TC: O(m × n)
-// Sc: O(m + n)
+// Optimal Approach
+// TC:  O(m × n)
+// SC: O(1)
 
 class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        boolean[] row = new boolean[m];
-        boolean[] col = new boolean[n];
+        boolean firstRow = false, firstCol = false;
+
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                
+
+                if(matrix[i][0] == 0) {
+                    firstRow = true;
+                }
+
+                if(matrix[0][j] == 0) {
+                    firstCol = true;
+                }
+            }
+        }
+        
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+
                 if(matrix[i][j] == 0) {
-                    row[i] = true;
-                    col[j] = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                
-                if(row[i] || col[j]) {
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+
+
+        if(firstRow) {
+            for(int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        if(firstCol) {
+            for(int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
             }
         }
     }
