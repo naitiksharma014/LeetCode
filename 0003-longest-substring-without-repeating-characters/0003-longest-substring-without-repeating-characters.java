a11251ee649119b9e91ef45ccf6ab047ba18a7e0
naitiksharma014 <naitiksharma014@gmail.com>
2026-04-24 12:29:13 +0530

    Refactor longest substring solution with multiple approaches

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,3 +1,70 @@
+//--------------------------------------------------------(Approach - 1)-------------------------------------------------------------
+// Sliding Window
+// TC: O(n²)
+// SC: O(n)
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        List<Character> list = new ArrayList<>();
+
+        int i = 0, j = 0;
+        int max = 0;
+
+        while(i < n) {
+
+            if(!list.contains(s.charAt(i))) {
+                
+                list.add(s.charAt(i));
+                max = Math.max(max, list.size());
+                i++;
+
+            } else {
+
+                list.remove(Character.valueOf(s.charAt(j)));
+                j++;
+            }
+        }
+
+        return max;
+    }
+}
+
+
+//--------------------------------------------------------(Approach - 2)-------------------------------------------------------------
+// Sliding Window + HashSet
+// TC: O(n)
+// SC: O(n)
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+
+        int i = 0, j = 0;
+        int max = 0;
+
+        while(i < n) {
+
+            if(!set.contains(s.charAt(i))) {
+                
+                set.add(s.charAt(i));
+                max = Math.max(max, set.size());
+                i++;
+
+            } else {
+
+                set.remove(s.charAt(j));
+                j++;
+            }
+        }
+
+        return max;
+    }
+}
+
+
+//--------------------------------------------------------(Approach - 3)-------------------------------------------------------------
 // Sliding Window + Hashmap
 // TC: O(n)
 // SC: O(n)
@@ -14,7 +81,7 @@ class Solution {
             char c = s.charAt(i);
 
             if(map.containsKey(c)) {
-                
+                    
                j = Math.max(j, map.get(c) + 1);
             }
 
