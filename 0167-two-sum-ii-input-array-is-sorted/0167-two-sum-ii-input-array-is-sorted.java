2ed89bf174c9c556e7609979610033da1dcf0bcb
naitiksharma014 <naitiksharma014@gmail.com>
2026-09-08 12:52:00 +0530

    Time: 3 ms (19.5%), Space: 48.2 MB (90.81%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,44 +1,27 @@
-// Outer loop: O(n)
-// Binary search for each element: O(log n)
-
-// Overall TC: O(n logn)
+// TC: O(n)
+// SC: O(1)
 
 class Solution {
-    public int binarySearch(int low, int t, int high, int[] numbers) {
+    public int[] twoSum(int[] numbers, int target) {
+        int n = numbers.length;
+        int i = 0, j = n - 1;
 
-        while(low <= high) {
+        while(i <= j) {
 
-            int mid = low + (high - low) / 2;
+            if(numbers[i] + numbers[j] == target) {
 
-            while(low < high && numbers[low] == numbers[low + 1]) low++;
-            while(low < high && numbers[high] == numbers[high - 1]) high--;
+                return new int[]{i + 1, j + 1};
 
-            if(numbers[mid] == t) {
-                return mid;
-            } else if(numbers[mid] < t) {
-                low = mid + 1;
+            } else if(numbers[i] + numbers[j] < target) {
+
+                i++;
+
             } else {
-                high = mid - 1;
+
+                j--;
             }
         }
 
-        return -1;
-    }
-
-    public int[] twoSum(int[] numbers, int target) {
-        int n = numbers.length;
-        int idx1 = -1, idx2 = -1;
-
-        for(int i = 0; i < n; i++) {
-
-            if(i != 0 && numbers[i] == numbers[i - 1]) continue;
-
-            idx1 = i;
-            idx2 = binarySearch(i + 1, target - numbers[i], n - 1, numbers);
-
-            if(idx2 != -1) break;
-        }
-
-        return new int[]{idx1 + 1, idx2 + 1};
+        return numbers;
     }
 }
