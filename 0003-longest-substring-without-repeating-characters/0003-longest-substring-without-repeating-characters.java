c384596e5dcab5ffe81113123893a8f56e79d6c8
naitiksharma014 <naitiksharma014@gmail.com>
2026-09-12 14:27:18 +0530

    Time: 64 ms (41.24%), Space: 48 MB (18.43%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,95 +1,26 @@
-//--------------------------------------------------------(Approach - 1)-------------------------------------------------------------
-// Sliding Window
-// TC: O(n²)
-// SC: O(n)
+// Time: O(n)
+// Space: O(min(n, charset))
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
-        List<Character> list = new ArrayList<>();
-
-        int i = 0, j = 0;
-        int max = 0;
-
-        while(i < n) {
-
-            if(!list.contains(s.charAt(i))) {
-                
-                list.add(s.charAt(i));
-                max = Math.max(max, list.size());
-                i++;
-
-            } else {
-
-                list.remove(Character.valueOf(s.charAt(j)));
-                j++;
-            }
-        }
-
-        return max;
-    }
-}
-
-
-//--------------------------------------------------------(Approach - 2)-------------------------------------------------------------
-// Sliding Window + HashSet
-// TC: O(n)
-// SC: O(n)
-
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
+        int maxLen = 0, left = 0;
         Set<Character> set = new HashSet<>();
 
-        int i = 0, j = 0;
-        int max = 0;
+        for(int right = 0; right < n; right++) {
 
-        while(i < n) {
+            char ch = s.charAt(right);
 
-            if(!set.contains(s.charAt(i))) {
-                
-                set.add(s.charAt(i));
-                max = Math.max(max, set.size());
-                i++;
-
-            } else {
-
-                set.remove(s.charAt(j));
-                j++;
-            }
-        }
-
-        return max;
-    }
-}
-
-
-//--------------------------------------------------------(Approach - 3)-------------------------------------------------------------
-// Sliding Window + Hashmap
-// TC: O(n)
-// SC: O(n)
-
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        Map<Character, Integer> map = new HashMap<>();
-
-        int i = 0, j = 0;
-        int max = 0;
-
-        while(i < n) {
-            char c = s.charAt(i);
-
-            if(map.containsKey(c)) {
-                    
-               j = Math.max(j, map.get(c) + 1);
+            while(set.contains(ch)) {
+                char leftCh = s.charAt(left);
+                set.remove(leftCh);
+                left++;
             }
 
-            map.put(c, i);
-            max = Math.max(max, i - j + 1);
-            i++;
+            set.add(ch);
+            maxLen = Math.max(maxLen, set.size());
         }
 
-        return max;
+        return maxLen;
     }
 }
